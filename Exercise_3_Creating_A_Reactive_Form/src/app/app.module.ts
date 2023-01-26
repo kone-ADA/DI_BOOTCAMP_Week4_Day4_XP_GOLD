5f4cdb57d0099b78571d0df70f39702bf3e946d7
kone-yaya-dev <64926396+kone-yaya-dev@users.noreply.github.com>
2023-01-26 09:48:45 +0000

    mis a jour exo3

--- Exercise_3_Creating_A_Reactive_Form/src/app/app.module.ts
+++ Exercise_3_Creating_A_Reactive_Form/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
